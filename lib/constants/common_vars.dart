ca9c93dc5ade1a1a016a7cf19d9963eccb0cf926
Edward-Kwak <sunny158@naver.com>
2021-07-15 10:04:42 +0900

    Added Customized loading widget.

---
+++ lib/constants/common_vars.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+const double common_padding_s = 8.0;
+const double common_padding_m = 12.0;
+const double common_padding_l = 24.0;
+
+const double common_loading_size = 160;
+
+Size? size;
--- lib/widgets/post.dart
+++ lib/widgets/post.dart
@@ -1,5 +1,7 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_code_split/constants/common_vars.dart';
+import 'package:flutter_code_split/widgets/custom_progress_indicator.dart';
 
 class Post extends StatelessWidget {
   final int index;
@@ -21,6 +23,9 @@ class Post extends StatelessWidget {
                     image: imageProvider,
                     fit: BoxFit.cover)),),
         );},
+      placeholder: (BuildContext context, String url) {
+        return CustomProgressIndicator(containerSize: size!.width,);
+      },
     );
   }
 }

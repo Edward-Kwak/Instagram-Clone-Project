e221cc332d7d9b3b55f1a867667f5d4453f80fee
Edward-Kwak <sunny158@naver.com>
2021-07-26 09:41:48 +0900

    Added _tabButtons Widget in profile_body.dart

--- lib/widgets/profile_body.dart
+++ lib/widgets/profile_body.dart
@@ -1,8 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_code_split/constants/common_vars.dart';
 
-class ProfileBody extends StatelessWidget {
+class ProfileBody extends StatefulWidget {
   const ProfileBody({Key? key}) : super(key: key);
+
+  @override
+  _ProfileBodyState createState() => _ProfileBodyState();
+}
+
+class _ProfileBodyState extends State<ProfileBody> {
+  bool selectedLeft = true;
 
   @override
   Widget build(BuildContext context) {
@@ -13,12 +20,38 @@ class ProfileBody extends StatelessWidget {
               delegate: SliverChildListDelegate([
                 _username(),
                 _userBio(),
-                _editProfileButton()
+                _editProfileButton(),
+                _tabButtons(),
               ]),
             )
           ],
         )
     );
+  }
+
+  Row _tabButtons() {
+    return Row(
+                children: <Widget>[
+                  Expanded(
+                      child: IconButton(
+                        onPressed: () {
+                          setState(() {
+                            selectedLeft = true;
+                          });
+                        },
+                        icon: ImageIcon(AssetImage('assets/images/grid.png'), color: selectedLeft? Colors.black : Colors.black26,),)
+                  ),Expanded(
+                      child: IconButton(
+                        onPressed: () {
+                          setState(() {
+                            selectedLeft = false;
+                          });
+                        },
+                        icon: ImageIcon(AssetImage('assets/images/saved.png'), color: selectedLeft? Colors.black26 : Colors.black,),)
+                  ),
+
+                ],
+              );
   }
 
   Widget _username() {

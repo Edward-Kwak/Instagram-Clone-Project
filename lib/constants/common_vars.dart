81a76ba2f5041eb402ababaa21641bc2d2cc7015
Edward-Kwak <sunny158@naver.com>
2021-07-27 10:02:49 +0900

    Added animated indicator on profile screen.

--- lib/constants/common_vars.dart
+++ lib/constants/common_vars.dart
@@ -14,3 +14,5 @@ const double common_img_icon_size_s = 16.0;
 const double common_icon_width = 44.0;
 
 const double common_radius = 6.0;
+
+const common_animation_duration = Duration(milliseconds: 300);

315b1d3dec98d3e24c64d17112ba927dfc07e615
Edward-Kwak <sunny158@naver.com>
2021-08-02 11:19:30 +0900

    Added Menu Animation on Profile Screen.

--- lib/screens/profile_screen.dart
+++ lib/screens/profile_screen.dart
@@ -2,35 +2,60 @@ import 'package:flutter/material.dart';
 import 'package:flutter_code_split/constants/common_vars.dart';
 import 'package:flutter_code_split/widgets/profile_body.dart';
 
-class ProfileScreen extends StatelessWidget {
+enum MenuStatus {opened, closed}
+
+class ProfileScreen extends StatefulWidget {
+
   const ProfileScreen({Key? key}) : super(key: key);
+
+  @override
+  _ProfileScreenState createState() => _ProfileScreenState();
+}
+
+class _ProfileScreenState extends State<ProfileScreen> {
+  MenuStatus _menuStatus = MenuStatus.closed;
+  final menuWidth = size!.width * 2 / 3;
+  double bodyXPos = 0;
+  double menuXPos = size!.width;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.grey[100],
-      body: SafeArea(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            _appbar(),
-            ProfileBody(),
-        ],),
+      body: Stack(
+          children: [
+            AnimatedContainer(
+                duration: common_animation_duration,
+                curve: Curves.fastOutSlowIn,
+                transform: Matrix4.translationValues(bodyXPos, 0, 0),
+                child: ProfileBody(onMenuChanged: () {
+                  setState(() {
+                    _menuStatus = (_menuStatus == MenuStatus.closed) ? MenuStatus.opened : MenuStatus.closed;
+
+                    switch (_menuStatus) {
+                      case MenuStatus.opened:
+                        bodyXPos -= menuWidth;
+                        menuXPos -= menuWidth;
+                        break;
+                      case MenuStatus.closed:
+                        bodyXPos = 0;
+                        menuXPos = size!.width;
+                        break;
+                    }
+                  });
+                }),
+            ),
+            AnimatedContainer(
+                duration: common_animation_duration,
+                curve: Curves.fastOutSlowIn,
+                transform: Matrix4.translationValues(menuXPos, 0, 0),
+                child: Container(
+                  width:menuWidth,
+                  color: Colors.blue,
+                )
+            )
+          ]
       ),
     );
   }
-
-  Row _appbar() {
-    return Row(children: <Widget>[
-      SizedBox(
-          width: common_icon_width,
-          child: IconButton(
-            onPressed: () {  },
-            icon: Icon(Icons.arrow_back),)),
-      Expanded(child: Text('user', textAlign: TextAlign.center, style: TextStyle(fontWeight: FontWeight.bold),)),
-    ],);
-  }
-
-
-
 }

d61493a5548aefe3e558520b3ce05a0381c6e94a
Edward K <sunny158@naver.com>
2021-08-06 18:26:57 +0900

    Debugging works done. FB Login & Follow/Unfollow & take_photo,video widget issues solved.

--- lib/widgets/take_video.dart
+++ lib/widgets/take_video.dart
@@ -1,9 +1,9 @@
 import 'dart:io';
-
 import 'package:camera/camera.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:image_gallery_saver/image_gallery_saver.dart';
 import 'package:make_feed_screen/constants/screen_size.dart';
 import 'package:make_feed_screen/models/camera_state.dart';
 import 'package:make_feed_screen/models/firestore/user_model_state.dart';
@@ -24,7 +24,7 @@ class TakeVideo extends StatefulWidget {
 class _TakeVideoState extends State<TakeVideo> {
 
   Widget _progress = MyProgressIndicator();
-  bool stopRec = true;
+  bool isRecording = false;
 
 
   @override
@@ -34,27 +34,30 @@ class _TakeVideoState extends State<TakeVideo> {
         return Column(
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            Container(
-              width: size!.width,
-              height: size!.width,
-              color: Colors.black,
-              child: (cameraState.isReadyToTakeVideo) ?  _getPreview(cameraState): _progress,
+            Flexible(
+              flex: 2,
+              fit: FlexFit.loose,
+              child: Container(
+                width: size!.width,
+                height: size!.width,
+                color: Colors.black,
+                child: (cameraState.isReadyToTakeVideo) ?  _getPreview(cameraState): _progress,
+              ),
             ),
-            Expanded(
-                child: IconButton(
-                  onPressed: (){
-                    setState(() {
-                      stopRec = !stopRec;
-                    });
-                    _attemptTakeVideo(cameraState, context, stopRec);
-
-                  },
-                  icon: Icon(
-                    Icons.fiber_manual_record_outlined,
-                    color: Colors.redAccent,
-                  ),
-                  iconSize: size!.width/4,
-                )
+            Flexible(
+              fit: FlexFit.tight,
+              child: IconButton(
+                onPressed: (){
+                  setState(() {
+                    isRecording = !isRecording;
+                  });
+                  _attemptTakeVideo(context, cameraState);
+                },
+                icon: isRecording ?
+                Icon(Icons.stop_circle_outlined, color: Colors.redAccent,)
+                    : Icon(Icons.fiber_manual_record_outlined, color:Colors.redAccent),
+                iconSize: size!.width/4,
+              ),
             ),
           ],
         );
@@ -77,77 +80,37 @@ class _TakeVideoState extends State<TakeVideo> {
     );
   }
 
-  void _attemptTakeVideo(CameraState cameraState, BuildContext context, bool stopFlag) async{
+  void _attemptTakeVideo(BuildContext context, CameraState cameraState) async{
     final String postKey = getNewPostKey(Provider.of<UserModelState>(context, listen: false).userModel!);
 
-    print('*****************************video mode :'+postKey);
+    print('***************************** Video Screen, PostKey is :'+postKey);
 
-    if(stopFlag) { /// stop Record
-      try {
-        // await cameraState.controller.prepareForVideoRecording();
+    if (isRecording) {
+      cameraState.controller.startVideoRecording();
+      SimpleSnackBar(context, '영상 촬영을 시작합니다.');
+      print('************************************************* before recording,');
+      await Future.delayed(Duration(seconds: 15));
+      print('************************************************* after recording,');
 
-        final XFile takenVideo = await cameraState.controller.stopVideoRecording();
+      final XFile result = await cameraState.controller.stopVideoRecording();
 
-        print('XFile taken video is :');
-        print(takenVideo);
+      SimpleSnackBar(context, '영상 촬영을 중단합니다.');
 
+      print('XFile taken video is :');
+      print(result);
 
-        final String path = takenVideo.path;
-        print("Taken Video path is : $path");
-        await takenVideo.saveTo(path);
+      final String resultPath = result.path;
+      print("Taken Video path is : $resultPath");
+      // await vid.saveTo(vid_path);
 
-        // Directory outPutPath = await
-
-        // Directory appDocDir = await getApplicationDocumentsDirectory();
-        // Directory? appDocDir = await getDownloadsDirectory();
-        // Directory appDocDir = await getApplicationSupportDirectory();
-        // Directory? appDocDir = await getExternalStorageDirectory();
-
-
-        // Directory savePath = await appDocDir!.create(recursive: true);
-        //
-        // print('**********************************app Doc Dir is  :' + appDocDir.toString());
-        //
-        // print('save path : ' + savePath.toString());
-        //
-        // String realSavePath = savePath.toString().substring(12,savePath.toString().length-1) + '/';
-        //
-        // print('Real Save Path is : '+realSavePath);
-
-        // await takenVideo.saveTo(realSavePath);
-
-        SimpleSnackBar(context, '영상 저장 완료 !' + path);
-        print('******************************************Save Completed !!!');
-
-        // File imageFile = File(path);
-        // Navigator.of(context).push(MaterialPageRoute(builder: (_) => SharePostScreen(imageFile, postKey: postKey,)));
-
-        // final XFile takenPic =
-        // final String path = takenPic.path;
-
-        // print("Taken Picture path is : $path");
-
-        // File imageFile = File(path);
-        // File imageFile = await File(path).create(recursive: true);
-
-        // Navigator.of(context).push(MaterialPageRoute(builder: (_) => SharePostScreen(imageFile, postKey: postKey,)));
-
-      } catch (e) {
-
-      }
+      final savedPath = await ImageGallerySaver.saveFile(resultPath);
+      print('************************************************* saved Path is : $savedPath');
+      SimpleSnackBar(context, '영상 저장 완료 !!\n$savedPath');
     }
     else {
-      try {
-        cameraState.controller.startVideoRecording();
-
-        SimpleSnackBar(context, '영상 촬영을 시작합니다.');
-
-
-      }catch(e) {
-
-      }
 
     }
+
 
   }
 
